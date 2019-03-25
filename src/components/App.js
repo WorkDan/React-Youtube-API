d00a2ed370bfc92d3d68a81a2360b9ca4e01a9ea
Daniel <saifulindaniel@gmail.com>
2019-03-25 12:24:41 +0600

    Fixes

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
       }
     })
 
-    this.setState({ videos: response.data.items })
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   }
 
   onVideoSelect = (video) => {
@@ -25,11 +28,19 @@ class App extends React.Component {
     return(
       <div className="ui container">
         <SearchBar onMyFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
